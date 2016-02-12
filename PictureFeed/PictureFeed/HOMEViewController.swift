9b227ddbcc626fc17217ecda9842fbe3c3d68fdf
Andy Malik <me@andymalik.com>
2016-02-12 08:34:43 -0800

    Xcode Created

---
+++ PictureFeed/PictureFeed/HOMEViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  PictureFeed
+//
+//  Created by Andy Malik on 2/12/16.
+//  Copyright © 2016 Andy Malik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
