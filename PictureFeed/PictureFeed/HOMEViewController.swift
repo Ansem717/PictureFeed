5027358a848bfc70a9d8a9781a0fcd383d206117
Andy Malik <me@andymalik.com>
2016-02-12 09:36:56 -0800

    Added UIAlertController and UIImagePickerController

--- PictureFeed/PictureFeed/HOMEViewController.swift
+++ PictureFeed/PictureFeed/HOMEViewController.swift
@@ -8,16 +8,64 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HOMEViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate
+{
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    lazy var UIIPC = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+    
+    func presentImagePicker(sourceType: UIImagePickerControllerSourceType)
+{
+        self.UIIPC.delegate = self
+        self.UIIPC.sourceType = sourceType
+        self.presentViewController(self.UIIPC, animated: true, completion: nil)
+    }
+    
+    func presentActionSheet() {
+        let actionSheet = UIAlertController(title: "Source", message: "Please select the source type.", preferredStyle: .ActionSheet)
+        let camAct = UIAlertAction(title: "Camera", style: .Default) { (action) -> Void in
+            self.presentImagePicker(.Camera)
+        }
+        let photoAct = UIAlertAction(title: "Photo", style: .Default) { (action) -> Void in
+            self.presentImagePicker(.PhotoLibrary)
+        }
+        let cancel = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+        actionSheet.addAction(camAct)
+        actionSheet.addAction(photoAct)
+        actionSheet.addAction(cancel)
+        self.presentViewController(actionSheet, animated: true, completion: nil)
+    }
 
-
+    @IBAction func addImage(sender: AnyObject) {
+        if UIImagePickerController.isSourceTypeAvailable(.Camera) {
+            presentActionSheet()
+        } else {
+            self.presentImagePicker(.PhotoLibrary)
+        }
+    }
 }
 
+extension HOMEViewController //MARK: Delegate Functions
+{
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+        self.imageView.image = image
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+}
