b980669723ad7619543ba51eb398e19e226d4e2a
Andy Malik <me@andymalik.com>
2016-02-16 17:52:57 -0800

    Refracted slightly refractored Tuesday's code.

--- PictureFeed/PictureFeed/HOMEViewController.swift
+++ PictureFeed/PictureFeed/HOMEViewController.swift
@@ -20,8 +20,6 @@ class HOMEViewController: UIViewController, UIImagePickerControllerDelegate, UIN
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let filterNames = CIFilter.filterNamesInCategories(nil) as [String]
-        print(filterNames)
     }
     
     override func viewDidAppear(animated: Bool) {
@@ -41,18 +39,17 @@ class HOMEViewController: UIViewController, UIImagePickerControllerDelegate, UIN
     
     func presentActionSheet() {
         
-        let actionSheet = UIAlertController(title: "Source", message: "Please select the source type.", preferredStyle: .ActionSheet)
-        let camAct = UIAlertAction(title: "Camera", style: .Default) { (action) -> Void in
+        let popUp = UIAlertController(title: "Source", message: "Please select the source type.", preferredStyle: .ActionSheet)
+        
+        popUp.addAction(UIAlertAction(title: "Camera", style: .Default) { (action) -> Void in
             self.presentImagePicker(.Camera)
-        }
-        let photoAct = UIAlertAction(title: "Photo", style: .Default) { (action) -> Void in
+            })
+        popUp.addAction(UIAlertAction(title: "Photo", style: .Default) { (action) -> Void in
             self.presentImagePicker(.PhotoLibrary)
-        }
-        let cancel = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
-        actionSheet.addAction(camAct)
-        actionSheet.addAction(photoAct)
-        actionSheet.addAction(cancel)
-        self.presentViewController(actionSheet, animated: true, completion: nil)
+            })
+        popUp.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
+        
+        self.presentViewController(popUp, animated: true, completion: nil)
     }
     
     
@@ -69,16 +66,19 @@ class HOMEViewController: UIViewController, UIImagePickerControllerDelegate, UIN
     @IBAction func editImage(sender: UIBarButtonItem) {
         
         guard let image = self.imageView.image else {
-            let alert = UIAlertController(title: "Hold on!", message: "You have not selected an image!", preferredStyle: .Alert)
-            alert.addAction(UIAlertAction(title: "Ok", style: .Cancel, handler: nil))
+            let popUp = UIAlertController(title: "Hold on!", message: "You have not selected an image!", preferredStyle: .Alert)
             
-            self.presentViewController(alert, animated: true, completion: nil)
+            popUp.addAction(UIAlertAction(title: "Ok", style: .Cancel, handler: nil))
+            
+            self.presentViewController(popUp, animated: true, completion: nil)
             
             return
         }
         
-        let actionSheet = UIAlertController(title: "Filters", message: "Please select a filter.", preferredStyle: .Alert)
-        let bwAction = UIAlertAction(title: "Black and White", style: .Default) { (action) -> Void in
+        let popUp = UIAlertController(title: "Filters", message: "Please select a filter.", preferredStyle: .Alert)
+        
+        //MARK: Possible to refractor with loop, maybe?
+        popUp.addAction(UIAlertAction(title: "Black and White", style: .Default) { (action) -> Void in
             self.activityIndicator.startAnimating()
             self.imageView.alpha = 0.5
             Filters.monochrome(image, completion: { (theImage) -> () in
@@ -86,8 +86,8 @@ class HOMEViewController: UIViewController, UIImagePickerControllerDelegate, UIN
                 self.activityIndicator.stopAnimating()
                 self.imageView.alpha = 1.0
             })
-        }
-        let crystalizeAction = UIAlertAction(title: "Crystalize", style: .Default) { (action) -> Void in
+        })
+        popUp.addAction(UIAlertAction(title: "Crystalize", style: .Default) { (action) -> Void in
             self.activityIndicator.startAnimating()
             self.imageView.alpha = 0.5
             Filters.crystalize(image, completion: { (theImage) -> () in
@@ -95,8 +95,8 @@ class HOMEViewController: UIViewController, UIImagePickerControllerDelegate, UIN
                 self.activityIndicator.stopAnimating()
                 self.imageView.alpha = 1.0
             })
-        }
-        let sepiaAction = UIAlertAction(title: "Sepia", style: .Default) { (action) -> Void in
+        })
+        popUp.addAction(UIAlertAction(title: "Sepia", style: .Default) { (action) -> Void in
             self.activityIndicator.startAnimating()
             self.imageView.alpha = 0.5
             Filters.sepia(image, completion: { (theImage) -> () in
@@ -104,8 +104,8 @@ class HOMEViewController: UIViewController, UIImagePickerControllerDelegate, UIN
                 self.activityIndicator.stopAnimating()
                 self.imageView.alpha = 1.0
             })
-        }
-        let bumpDistortionAction = UIAlertAction(title: "Bump Distortion", style: .Default) { (action) -> Void in
+        })
+        popUp.addAction(UIAlertAction(title: "Bump Distortion", style: .Default) { (action) -> Void in
             self.activityIndicator.startAnimating()
             self.imageView.alpha = 0.5
             Filters.bumpDistortion(image, completion: { (theImage) -> () in
@@ -113,8 +113,8 @@ class HOMEViewController: UIViewController, UIImagePickerControllerDelegate, UIN
                 self.activityIndicator.stopAnimating()
                 self.imageView.alpha = 1.0
             })
-        }
-        let colorPosterizeAction = UIAlertAction(title: "Color Posterize", style: .Default) { (action) -> Void in
+        })
+        popUp.addAction(UIAlertAction(title: "Color Posterize", style: .Default) { (action) -> Void in
             self.activityIndicator.startAnimating()
             self.imageView.alpha = 0.5
             Filters.colorPosterize(image, completion: { (theImage) -> () in
@@ -122,45 +122,38 @@ class HOMEViewController: UIViewController, UIImagePickerControllerDelegate, UIN
                 self.activityIndicator.stopAnimating()
                 self.imageView.alpha = 1.0
             })
-        }
+        })
         
-        let resetAction = UIAlertAction(title: "Reset", style: .Destructive) { (action) -> Void in
+        popUp.addAction(UIAlertAction(title: "Reset", style: .Destructive) { (action) -> Void in
             self.imageView.image = self.originalImage.image
-        }
+        })
         
-        let cancel = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+        popUp.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
         
-        actionSheet.addAction(bwAction)
-        actionSheet.addAction(crystalizeAction)
-        actionSheet.addAction(sepiaAction)
-        actionSheet.addAction(bumpDistortionAction)
-        actionSheet.addAction(colorPosterizeAction)
-        actionSheet.addAction(resetAction)
-        actionSheet.addAction(cancel)
-        
-        self.presentViewController(actionSheet, animated: true, completion: nil)
+        self.presentViewController(popUp, animated: true, completion: nil)
         
     }
     
     @IBAction func saveImage(sender: UIBarButtonItem) {
         
         guard let image = self.imageView.image else {
-            let alert = UIAlertController(title: "Hold on!", message: "You have not selected an image!", preferredStyle: .Alert)
-            alert.addAction(UIAlertAction(title: "Ok", style: .Cancel, handler: nil))
+            let popUp = UIAlertController(title: "Hold on!", message: "You have not selected an image!", preferredStyle: .Alert)
+            popUp.addAction(UIAlertAction(title: "Ok", style: .Cancel, handler: nil))
             
-            self.presentViewController(alert, animated: true, completion: nil)
+            self.presentViewController(popUp, animated: true, completion: nil)
             
             return
         }
         
         if image == originalImage.image {
-            let alert = UIAlertController(title: "Hold on!", message: "Are you sure you wish to save an identical image?", preferredStyle: .Alert)
-            alert.addAction(UIAlertAction(title: "Continue", style: .Default, handler: { (action) -> Void in
-                self.proceedToSave(image)
-            }))
-            alert.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
+            let popUp = UIAlertController(title: "Hold on!", message: "Are you sure you wish to save an identical image?", preferredStyle: .Alert)
             
-            self.presentViewController(alert, animated: true, completion: nil)
+            popUp.addAction(UIAlertAction(title: "Continue", style: .Default) { (action) -> Void in
+                self.proceedToSave(image)
+                })
+            popUp.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
+            
+            self.presentViewController(popUp, animated: true, completion: nil)
             
             return
         }
@@ -169,11 +162,11 @@ class HOMEViewController: UIViewController, UIImagePickerControllerDelegate, UIN
     }
     
     func proceedToSave(image: UIImage) {
-        //Add prompt for status
+        //MARK: Add prompt for status
         let status = "Check out my filters!"
         self.activityIndicator.startAnimating()
         self.imageView.alpha = 0.5
-    
+        
         API.shared.POST(Post(image: image, status: status)) { (success) -> () in
             if success {
                 UIImageWriteToSavedPhotosAlbum(image, self, "image:didFinishSavingWithError:contextInfo:", nil)
@@ -186,9 +179,11 @@ class HOMEViewController: UIViewController, UIImagePickerControllerDelegate, UIN
         self.imageView.alpha = 1.0
         if error == nil {
             
-            let alertController = UIAlertController(title: "Saved!", message: "Your image has been saved to your photos", preferredStyle: .Alert)
-            alertController.addAction(UIAlertAction(title: "Ok", style: .Default, handler: nil))
-            self.presentViewController(alertController, animated: true, completion: nil)
+            let popUp = UIAlertController(title: "Saved!", message: "Your image has been saved to your photos", preferredStyle: .Alert)
+            
+            popUp.addAction(UIAlertAction(title: "Ok", style: .Default, handler: nil))
+            
+            self.presentViewController(popUp, animated: true, completion: nil)
         }
         
         print("Line 201 HOMEViewController ERROR - \(error)")
